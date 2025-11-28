88b5540bdfb3b71fc19b08ed72e51af88e369fe2
José Velázquez Expósito <josevelazquezexposito@gmail.com>
2025-11-28 09:49:12 +0100

    Update projects-config.js

--- projects-config.js
+++ projects-config.js
@@ -53,7 +53,7 @@ const projects = [
     },
      {
         id: 2,
-        title: "Próximamente: Aplicación Web con Angular desplegada en AWS...",
+        title: "⏳ Próximamente: Aplicación Web con Angular desplegada en AWS...",
         description: "Muestra cómo servir sitios web estáticos o SPA en la nube",
         fullDescription: "Sitio web estático Angular alojado en Amazon S3, distribuido globalmente con CloudFront y protegido con HTTPS mediante AWS Certificate Manager.",
         image: "images/App_Web_Desplegada_AWS.jpg",
